31a06e94e9d0721778b197cba49f56dd1946e288
unknown <FAMHURP@FM-HBVIR-AC-C08.urp.edu.pe>
2026-05-25 09:23:59 -0500

    actualizacion de mapa

--- Backend/obj/Debug/net8.0/ControlLaboratorio.API.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/ControlLaboratorio.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlLaboratorio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edee6c3135aaa836e766d328bbd558211ace829a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618d71b1e8cf31c86fde13a7e7d112414462ad17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlLaboratorio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlLaboratorio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
